6c29b72b1e0bc651e5769b82c3c94864211ee7a6
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2022-08-01 17:12:02 -0400

    Atualizações

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import "react-native-gesture-handler";
-import "intl";
-import "intl/locale-data/jsonp/pt-BR";
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import { useFonts } from "expo-font";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import 'react-native-gesture-handler';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { useFonts } from 'expo-font';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import {
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
-import theme from "./src/global/styles/theme";
-import { NavigationContainer } from "@react-navigation/native";
-import AppRoutes from "./src/routes/app.routes";
-import { StatusBar } from "react-native";
-import SignIn from "./src/screens/SignIn";
+} from '@expo-google-fonts/poppins';
+import theme from './src/global/styles/theme';
+import { NavigationContainer } from '@react-navigation/native';
+import AppRoutes from './src/routes/app.routes';
+import { StatusBar } from 'react-native';
+import SignIn from './src/screens/SignIn';
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
