60e3982e917214cf3dfc850cbeb79e2d0317b8e8
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2022-08-07 19:25:32 -0400

    Conectando com uma conta google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, ReactNode, useContext } from 'react';
+import * as AuthSession from "expo-auth-session";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -13,21 +14,41 @@ interface User {
 
 interface IAuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>;
 }
 
 export const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const user = {
-    id: '2',
-    name: 'Fabio Silva',
-    email: 'fabioss930@gmail.com',
-    photo: '',
+    id: "2",
+    name: "Fabio Silva",
+    email: "fabioss930@gmail.com",
+    photo: "",
   };
+
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID =
+        "204314563646-7e1jh3vbt61chei5vt14s8b1ikpnepvk.apps.googleusercontent.com";
+
+      const REDIRECT_URI = "https://auth.expo.io/fabio.dev/gofinances";
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+      const response = AuthSession.startAsync({ authUrl });
+      console.log(response);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   return (
     <AuthContext.Provider
       value={{
         user,
+        signInWithGoogle,
       }}
     >
       {children}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -14,29 +14,43 @@ import {
   Footer,
   FooterWrapper,
 } from './styles';
+import { Alert } from "react-native";
 
 const SignIn: React.FC = () => {
-  const { user } = useAuth();
-  console.log(user);
+  const { signInWithGoogle } = useAuth();
+
+  async function handleDignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Atenção!", "Não foi possível conectar a conta Google.");
+    }
+  }
+
   return (
     <Container>
       <Header>
         <TitleWrapper>
           <LogoSvg width={RFValue(120)} height={RFValue(68)} />
           <Title>
-            Controle suas {'\n'}
-            finanças de forma {'\n'}
+            Controle suas {"\n"}
+            finanças de forma {"\n"}
             muito simples
           </Title>
         </TitleWrapper>
         <SignInTitle>
-          Faça seu login com {'\n'}
+          Faça seu login com {"\n"}
           uma das contas abaixo
         </SignInTitle>
       </Header>
       <Footer>
         <FooterWrapper>
-          <SignSocialButton title="Entrar com Google" svg={GoogleSvg} />
+          <SignSocialButton
+            title="Entrar com Google"
+            svg={GoogleSvg}
+            onPress={handleDignInWithGoogle}
+          />
           <SignSocialButton title="Entrar com Apple" svg={AppleSvg} />
         </FooterWrapper>
       </Footer>
